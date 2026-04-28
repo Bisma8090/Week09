df16811af9e9370912eee257c17e0c8c4a85c9e7
Bisma8090 <bismaarshad8090@gmail.com>
2026-04-28 15:11:54 +0500

    Updation

---
+++ ResearchAssisstant/backend/api/index.ts
@@ -0,0 +1,16 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from '../src/app.module';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
+
+const server = express();
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+  app.enableCors({ origin: '*' });
+  await app.init();
+}
+
+bootstrap();
+
+export default server;
